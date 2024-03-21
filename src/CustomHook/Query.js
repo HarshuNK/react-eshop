451c3ad50ce4cf98dc35dc2f9d811941783b8bbc
HarshuNK <harshithank58@gmail.com>
2024-03-21 14:03:52 +0530

    modified

---
+++ src/CustomHook/Query.js
@@ -0,0 +1,9 @@
+/* custom hook */
+import React from "react";
+import { useLocation } from "react-router-dom"; 
+function useQuery() {
+    const { search } = useLocation()
+
+    return React.useMemo(() => new  URLSearchParams(search), [search])
+}
+export { useQuery }
--- src/Screen/Product/ProductItem.jsx
+++ src/Screen/Product/ProductItem.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function ProductItem(props) {
     let { id, title, image, category, description, price, rating} = props
   return (
     <div className="card">
-        <div className="card-front">
-            <img src={image?image:''} alt="" className='card-img' />
-            <div className="card-content">
-                <div className="title-sec">
-                    <h6 className="card-title"> {title} </h6>
-                    <p className="price"> &#8377; {price} </p>
+        <NavLink to={`/product/${id}`} style={{ textDecoration: 'none'}} > 
+            <div className="card-front">
+                <img src={image?image:''} alt="" className='card-img' />
+                <div className="card-content">
+                    <div className="title-sec">
+                        <h6 className="card-title"> {title} </h6>
+                        <p className="price"> &#8377; {price} </p>
+                    </div>
+                    <p className="category">{category}</p>
                 </div>
-                <p className="category">{category}</p>
             </div>
-        </div>
+        </NavLink>
         <div className="card-back">
             <button className="btn">Add to Cart
                 <i className="bi bi-cart"></i>
